021717c1a8053dadd7f26734068feb3758203720
Srianjani1234 <srianjani.9704@gmail.com>
2025-07-18 20:05:25 +0530

    Fix: add Render domain to ALLOWED_HOSTS

--- Bank/settings.py
+++ Bank/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(" ")
+ALLOWED_HOSTS = ["bank-uw21.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 
